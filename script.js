ba9950f62ec2e62759e7d2fc90df4f01d45e7595
gabriel-zanotti <luv.neworder@gmail.com>
2024-01-31 15:22:37 -0300

    up

---
+++ script.js
@@ -0,0 +1,59 @@
+const nomeFaixa = document.getElementById("faixa");
+const audio = document.getElementById("musica");
+const botaoPlayPause = document.getElementById("play-pause");
+const botaoProximaFaixa = document.getElementById("proximo");
+const botaoFaixaAnterior = document.getElementById("anterior");
+
+const totalFaixas = 6;
+let taTocando = false;
+let faixa = 1;
+
+function tocarFaixa() {
+    botaoPlayPause.classList.remove("bi-play-circle-fill");
+    botaoPlayPause.classList.add("bi-pause-circle-fill");
+    audio.play();
+    taTocando = true;
+}
+
+function pausarFaixa() {
+    botaoPlayPause.classList.add("bi-play-circle-fill");
+    botaoPlayPause.classList.remove("bi-pause-circle-fill");
+    audio.pause();
+    taTocando = false;
+}
+
+function tocarOuPausarFaixa() {
+  if (taTocando === true) {
+    pausarFaixa();
+  } else {
+    tocarFaixa();
+  }
+}
+
+function faixaAnterior() {
+    if (faixa === 1) {
+      faixa = totalFaixas;
+    } else {
+      faixa -= 1;
+    }
+    audio.src = "music/vultures/" + faixa + ".mp3";
+    nomeFaixa.innerText = "Faixa " + faixa;
+    tocarFaixa();
+  }
+
+  function proximaFaixa() {
+    if (faixa < totalFaixas) {
+      faixa += 1;
+    } else {
+      faixa = 1;
+    }
+    audio.src = "music/vultures/" + faixa + ".mp3";
+    nomeFaixa.innerText = "Faixa " + faixa;
+    tocarFaixa();
+  }
+
+
+  botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
+  botaoFaixaAnterior.addEventListener("click", faixaAnterior);
+  botaoProximaFaixa.addEventListener("click", proximaFaixa);
+  audio.addEventListener("ended", proximaFaixa);
